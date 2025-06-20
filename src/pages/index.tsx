dadfb09a85153c127c9e20cb7b2d912baa0f52f0
dukang <dk@nosugartech.com>
2025-06-20 18:00:39 +0800

    feat: test Grid.useBreakpoint

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
-import yayJpg from '../assets/yay.jpg';
+import { Grid } from 'antd';
 
+const { useBreakpoint } = Grid;
 export default function HomePage() {
+  const { xs, sm, md, lg, xl, xxl } = useBreakpoint();
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
+    <div style={{ fontSize: 40 }}>
+      <ul>
+        <li>xs: {xs + ' <  576px'}</li>
+        <li>sm: {sm + ' >= 576px'}</li>
+        <li>md: {md + ' >= 768px'}</li>
+        <li>lg: {lg + ' >= 992px'}</li>
+        <li>xl: {xl + ' >= 1200px'}</li>
+        <li>xxl: {xxl + ' >= 1600px'}</li>
+      </ul>
     </div>
   );
 }
